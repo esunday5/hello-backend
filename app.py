868334673bdf0ee3d5cb46cf57c0bbd49fc21249
esunday5 <emmyblaq3@gmail.com>
2024-12-14 17:50:25 +0100

    first commit

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    """API route to send data to the Next.js frontend"""
+    return jsonify({"message": "Hello from Flask nnn!"})
